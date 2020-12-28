c663d03974c9bf0af8d876bf975611d2e579f857
Guillem <Guillem.Torrente@sony.com>
2020-12-28 15:34:42 +0900

    Fix python version variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ mkdir -p /home/ws/src
 cp -r "$GITHUB_WORKSPACE"/* /home/ws/src
 
 # Get Python version and major (e.g. python3.8)
-python_ver = $(python3 -c 'import sys; print("python%d.%d" % (sys.version_info[0], sys.version_info[1]))')
+python_ver=$(python3 -c 'import sys; print("python%d.%d" % (sys.version_info[0], sys.version_info[1]))')
 
 SET(ENV{PYTHONPATH} "/opt/ros/dashing/lib/${python_ver}/site-packages/")
 
