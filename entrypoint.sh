ed932ecb4a42c751cde6d59b8f93fe274ca88cbf
Guillem <Guillem.Torrente@sony.com>
2020-12-28 15:54:52 +0900

    Add chivatos

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 mkdir -p /home/ws/src
-cp -r "$GITHUB_WORKSPACE"/* /home/ws/src
+cp -r -v "$GITHUB_WORKSPACE"/* /home/ws/src
 
 # Get Python version and major (e.g. python3.8)
 python_ver=$(python3 -c 'import sys; print("python%d.%d" % (sys.version_info[0], sys.version_info[1]))')
@@ -11,5 +11,6 @@ python_ver=$(python3 -c 'import sys; print("python%d.%d" % (sys.version_info[0],
 cd /home/ws
 ls -la src
 pwd
+echo $PYTHONPATH
 
 colcon build
