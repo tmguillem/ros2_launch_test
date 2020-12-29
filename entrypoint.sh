67089c16a6c7bfcb0c858e3da5afa2ca1fd38eaa
Guillem <Guillem.Torrente@sony.com>
2020-12-29 16:21:27 +0900

    Record ros bag

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 set -e
-source "/opt/ros/$ROS_DISTRO/setup.bash"
-
 env
 
 mkdir -p $GITHUB_WORKSPACE/ws/src
@@ -16,13 +14,22 @@ cd ws
 # Set GUI_FLAG to False to be able to run the simulator in the container. TODO: pass this as an argument?
 sed -i 's/GUI_FLAG = True/GUI_FLAG = False/' src/simulator/bullet_simulation/bullet_simulation/full_simulator.py
 
+# Compile and source workspace packages
+source "/opt/ros/$ROS_DISTRO/setup.bash"
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
-
 source install/setup.bash
 
+# Run requested launchfile
 echo "Running in background: $INPUT_PACKAGE $INPUT_LAUNCHFILE"
 ros2 run $INPUT_PACKAGE $INPUT_LAUNCHFILE &
 
+# Record short rosbag (timeout after 5 seconds)
+ros2 bag record -a --include-hidden-topics -o health_check_bag &>/dev/null
+sleep 5 && kill -INT $!
+
+ros2 bag info health_check_bag
+
+# Check topics
 echo "Checking topics:"
 for word in $INPUT_LISTEN_TOPICS
 do
