e3157d1faa79aa9d8391c53a482e529d2b72dd0e
Guillem <Guillem.Torrente@sony.com>
2020-12-29 14:02:12 +0900

    Random guess

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,12 +10,11 @@ cd $GITHUB_WORKSPACE
 rsync -av --remove-source-files . ws/src --exclude ws
 
 cd ws
-ls -la src
 
 # Set GUI_FLAG to False to be able to run the simulator in the container. TODO: pass this as an argument?
 sed -i 's/GUI_FLAG = True/GUI_FLAG = False/' src/simulator/bullet_simulation/bullet_simulation/full_simulator.py
 
-colcon build
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
 
 source install/setup.bash
 
