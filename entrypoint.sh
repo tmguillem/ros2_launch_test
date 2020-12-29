4d5f0d8bd2751e0d2d536f79de822878ca504532
Guillem <Guillem.Torrente@sony.com>
2020-12-29 12:03:16 +0900

    Set GUI_FLAG to false in the container automatically

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,9 @@ source "/opt/ros/$ROS_DISTRO/setup.bash"
 mkdir -p /home/ws/src
 cp -r "$GITHUB_WORKSPACE" /home/ws/src
 
+# Set GUI_FLAG to False to be able to run the simulator in the container. TODO: pass this as an argument?
+sed -i 's/GUI_FLAG = True/GUI_FLAG = False/' src/project_ace/simulator/bullet_simulation/bullet_simulation/full_simulator.py
+
 cd /home/ws
 colcon build
 
