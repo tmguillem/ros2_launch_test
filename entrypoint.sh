34ef64a00025d9a1420e255e49f4d63914d3566d
Guillem <Guillem.Torrente@sony.com>
2020-12-29 12:08:50 +0900

    Debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ mkdir -p /home/ws/src
 cp -r "$GITHUB_WORKSPACE" /home/ws/src
 
 # Set GUI_FLAG to False to be able to run the simulator in the container. TODO: pass this as an argument?
-ls /home/ws/src
-sed -i 's/GUI_FLAG = True/GUI_FLAG = False/' /home/ws/src/project_ace/simulator/bullet_simulation/bullet_simulation/full_simulator.py
+ls /home/ws/src/$GITHUB_WORKSPACE
+sed -i 's/GUI_FLAG = True/GUI_FLAG = False/' /home/ws/src/$GITHUB_WORKSPACE/simulator/bullet_simulation/bullet_simulation/full_simulator.py
 
 cd /home/ws
 colcon build
