f86d092ff7dd22313c94b66ee61d6c27a95dd3ce
Guillem <Guillem.Torrente@sony.com>
2021-01-13 15:20:22 +0900

    Add project name folder into ws

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,18 @@
 set -e
 env
 
-mkdir -p $GITHUB_WORKSPACE/ws/src
+project_name=$(basename `git rev-parse --show-toplevel`)
+
+mkdir -p $GITHUB_WORKSPACE/ws/src/$project_name
 cd $GITHUB_WORKSPACE
 
 # Move all files inside ws/src
-rsync -aq --remove-source-files . ws/src --exclude ws
+rsync -aq --remove-source-files . ws/src/$project_name --exclude ws
 
 cd ws
 
 # Set GUI_FLAG to False to be able to run the simulator in the container. TODO: pass this as an argument?
-sed -i 's/GUI_FLAG = True/GUI_FLAG = False/' src/simulator/bullet_simulation/bullet_simulation/full_simulator.py
+sed -i 's/GUI_FLAG = True/GUI_FLAG = False/' src/$project_name/simulator/bullet_simulation/bullet_simulation/full_simulator.py
 
 # Compile and source workspace packages
 source "/opt/ros/$ROS_DISTRO/setup.bash"
